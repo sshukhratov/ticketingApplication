291dafe7ba7925da92984a07a8f4e3e052c0a97b
sshukhratov <sshukhratov@gmail.com>
2022-03-24 20:26:52 -0500

    first init

---
+++ src/main/java/com/cydeo/service/RoleService.java
@@ -0,0 +1,12 @@
+package com.cydeo.service;
+
+import com.cydeo.dto.RoleDTO;
+import com.cydeo.dto.UserDTO;
+import com.cydeo.entity.Role;
+
+import java.util.List;
+
+public interface RoleService extends CrudService<RoleDTO, Long>{
+
+
+}
---
+++ src/main/java/com/cydeo/service/UserService.java
@@ -0,0 +1,12 @@
+package com.cydeo.service;
+
+import com.cydeo.dto.UserDTO;
+import com.cydeo.entity.User;
+
+import java.util.List;
+
+public interface UserService extends CrudService<UserDTO, String>{
+
+
+
+}
